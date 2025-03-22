bb71c679a8b966c9dd8d0856698f2f6b199fc9c5
othmane099 <othmabe99@gmail.com>
2025-03-22 12:04:34 +0100

    add dependency container

---
+++ src/containers.py
@@ -0,0 +1,10 @@
+from dependency_injector import containers
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        packages=[
+
+        ]
+    )
+
